975de9d2775d9e411601668203b13726f1a601e3
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-07-18 17:45:42 +0530

    p2

--- prac2.js
+++ prac2.js
@@ -1,3 +1,25 @@
-class student{
-    p2
+class student {
+    constructor(name, rollno) {
+       this.name=name
+       this.rno=rollno
+    }
+    details() {
+        console.log("This are the details about the Student")
+    }
+    Name(){
+        console.log("Your name is : "+ this.name)
+    }
+    RollNo(){
+        console.log("Your roll no is : "+ this.rno)
+    }
 }
+
+let Vaidehi = new student("Vaidehi",14)
+Vaidehi.details()
+Vaidehi.Name()
+Vaidehi.RollNo()
+
+let B = new student("Jeet",17)
+B.details()
+B.Name()
+B.RollNo()
