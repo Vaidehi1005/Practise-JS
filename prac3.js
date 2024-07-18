c8d6f98e7df0b5235651d980c152f55bcd9eda0f
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-07-18 18:03:48 +0530

    p3

---
+++ prac3.js
@@ -0,0 +1,9 @@
+class employee {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+let emp = new employee("Vinesh", 25);
+console.log(emp.name);
+console.log(emp.age);
