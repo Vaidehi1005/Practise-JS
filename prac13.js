6f090ba3e25a89e5d2d1b436c4e09b32a198eb4d
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-07-18 19:29:00 +0530

    p

--- prac13.js
+++ prac13.js
@@ -1,18 +1,18 @@
 class Rectangle {
     constructor(width, height) {
-      this.width = width;
-      this.height = height;
+        this.width = width;
+        this.height = height;
     }
-  
+
     getArea() {
-      return this.width * this.height;
+        return this.width * this.height;
     }
-  
+
     getPerimeter() {
-      return 2 * (this.width + this.height);
+        return 2 * (this.width + this.height);
     }
-  }
-  
-  let rectangle = new Rectangle(4, 5);
-  console.log(rectangle.getArea()); // 20
-  console.log(rectangle.getPerimeter()); // 18
+}
+
+let rectangle = new Rectangle(29, 5);
+console.log(rectangle.getArea());
+console.log(rectangle.getPerimeter());
