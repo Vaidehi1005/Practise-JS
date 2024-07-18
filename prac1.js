5b2812c6950bc887864e0127159fe6e2bf5df074
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-07-18 16:34:55 +0530

    practise

--- prac1.js
+++ prac1.js
@@ -1,6 +1,6 @@
 class RailwayForm {
     submit() {
-        alert("Form Submitted")
+        alert(this.name + ": Your Form Submitted")
     }
     cancel() {
         alert(this.name+ ": This form is cancelled");
@@ -9,10 +9,15 @@ class RailwayForm {
         this.name=givenname  //this.name k che k object ni property che name means aa object ni ek property associate thai jase name k ane enu naam je object aapse .fill(givenname) e name property ne assign karse je pn naam hase e 
     }
 }
-
+// create a form for Harry 
 let harryForm = new RailwayForm()
+// fill the form with Harrys's details
 harryForm.fill("Harry")
+
+
+// create a form for Rohan
 let rohanForm = new RailwayForm()
+// fill the form with Rohan's details
 rohanForm.fill("Rohan")
 
 harryForm.submit()
