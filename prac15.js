9cb4307272afa8ab354182d120ac0e9c8bf9f068
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-07-18 19:34:19 +0530

    p

---
+++ prac15.js
@@ -0,0 +1,19 @@
+class Employee {
+    constructor(name, salary) {
+      this.name = name;
+      this.salary = salary;
+    }
+  
+    getSalary() {
+      return this.salary;
+    }
+  
+    setSalary(salary) {
+      this.salary = salary;
+    }
+  }
+  
+  let employee = new Employee("John", 50000);
+  console.log(employee.getSalary()); 
+  employee.setSalary(60000);
+  console.log(employee.getSalary()); 
